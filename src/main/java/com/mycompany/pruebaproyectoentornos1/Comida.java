c5977e3935880acd827c1c4dde4004da5e9b6643
AndPiraquive <andrea.piraquive@gmail.com>
2025-01-14 13:00:32 +0100

    Update Comida.java
    Agregue código en la clase comida

--- src/main/java/com/mycompany/pruebaproyectoentornos1/Comida.java
+++ src/main/java/com/mycompany/pruebaproyectoentornos1/Comida.java
@@ -15,4 +15,17 @@ public class Comida {
         System.out.println("Escribe tu nombre: ");
         String nom = Teclado.nextLine();
     }
+
+    public void ejercicioCuatro() {
+        /*Escribe un programa en Java que convierta una temperatura de grados
+            Celsius a Fahrenheit. Usa la fórmula*/
+
+        Scanner Teclado = new Scanner(System.in);
+
+        System.out.println("Dime la temperatura que quieres convertir: ");
+        float temp = Teclado.nextFloat();
+        float fahrenheit = (temp * 9 / 5) + 32;
+
+        System.out.println("La temperatura es " + fahrenheit + " grados Fahrenheit");
+    }
 }
