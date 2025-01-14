e73457edee1b7f77f4f5fc4f806651b7a9dd2c89
AndPiraquive <andrea.piraquive@gmail.com>
2025-01-14 09:56:42 +0100

    Add files via upload

---
+++ src/main/java/com/mycompany/Comida.java
@@ -0,0 +1,18 @@
+package com.mycompany.pruebaproyectoentornos1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Lu Piraquive
+ */
+public class Comida {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        Scanner Teclado = new Scanner(System.in);
+        System.out.println("Escribe tu nombre: ");
+        String nom = Teclado.nextLine();
+    }
+}
