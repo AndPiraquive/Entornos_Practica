60a5854779d06d7fb61542c0851d552ab3f7ab87
Lu Piraquive <Lu Piraquive@192.168.1.153>
2025-01-14 01:14:06 +0100

    Iniciando la prueba

---
+++ src/main/java/com/mycompany/pruebaproyectoentornos1/Comida.java
@@ -0,0 +1,16 @@
+package com.mycompany.pruebaproyectoentornos1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Lu Piraquive
+ */
+public class Comida {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        Scanner Teclado = new Scanner(System.in);
+    }
+}
